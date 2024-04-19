9c34242197e062d7c73f7f6be7bb449da18bd214
Aaren Vo <aarenjvo@gmail.com>
2024-04-18 19:59:42 -0700

    added dark/light mode toggle

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+// connect to data
+import { useSelector, useDispatch, connect } from 'react-redux'
+// mutate the data
+import { setDarkTheme, setLightTheme } from './features/themeSlice'; 
 
 function App() {
+  const dispatch = useDispatch()
+  const theme = useSelector((state) => state.theme)
+  console.log("state from redux", theme)
   
+  // color1 is bg
+  // color5 is font
+
+  const handleOnClick = () => {
+    // if dark do this
+    if(theme.darkMode){
+    dispatch(setLightTheme())
+    } else {
+      dispatch(setDarkTheme())
+    }
+    // what to do if light?
+
+  }
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div style={{ backgroundColor: theme.color1, color: theme.color5 }} className="App">
+      <button onClick={handleOnClick}>Set Theme</button>
       <Nav />
       <ContentWrapper />
       <Footer />
